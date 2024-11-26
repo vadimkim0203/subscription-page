d78435cbe78f04f17fbc9f2731fc37a3c15591d9
Vadim Kim <vkim0203@gmail.com>
2024-11-26 16:27:40 +0900

    the layout for the future subscription page

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header/Header.jsx";
+import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+
+  function handleSelect(selectedButton) {
+    
+    console.log(selectedButton);
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => {handleSelect("components")}}>Components</TabButton>
+            <TabButton onSelect={() => {handleSelect("jsx")}}>JSX</TabButton>
+            <TabButton onSelect={() => {handleSelect("props")}}>Props</TabButton>
+            <TabButton onSelect={() => {handleSelect("state")}}>State</TabButton>
+          </menu>
+          Dynamic content
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,13 @@
+export function TabButton({children, onSelect}) {
+  
+ 
+
+  return(
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+
+}
+
+export default TabButton;
